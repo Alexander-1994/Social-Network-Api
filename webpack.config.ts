a65d3e911378e1a8675a072470647785b3b111f3
Alexander <kisliy.doma@gmail.com>
2025-01-12 20:47:49 +0800

    server: мелкие правки

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,5 @@
 import type { Configuration } from 'webpack';
 import path from 'path';
-import CopyPlugin from 'copy-webpack-plugin';
 
 const config: Configuration = {
   mode: 'production',
@@ -11,16 +10,6 @@ const config: Configuration = {
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'prisma/schema.prisma'),
-          to: path.resolve(__dirname, 'build', 'prisma/schema.prisma'),
-        },
-      ],
-    }),
-  ],
   module: {
     rules: [{ test: /\.ts/, loader: 'ts-loader', exclude: /node_modules/ }],
   },
